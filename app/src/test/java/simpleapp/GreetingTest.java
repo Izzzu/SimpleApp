49dd9176cce2c958fa18665c7eee593237262611
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-02-03 14:06:25 +0100

    change

--- app/src/test/java/simpleapp/GreetingTest.java
+++ app/src/test/java/simpleapp/GreetingTest.java
@@ -2,13 +2,13 @@ package simpleapp;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GreetingTest {
 
     @Test
     void appHasAGreeting() {
-
         Greeting classUnderTest = new Greeting();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
