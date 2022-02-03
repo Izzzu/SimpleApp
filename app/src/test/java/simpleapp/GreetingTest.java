c7dfcd5232620a447f966e81d8980ef2b268717f
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-02-03 14:00:10 +0100

    change

--- app/src/test/java/simpleapp/GreetingTest.java
+++ app/src/test/java/simpleapp/GreetingTest.java
@@ -8,6 +8,7 @@ class GreetingTest {
 
     @Test
     void appHasAGreeting() {
+
         Greeting classUnderTest = new Greeting();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
